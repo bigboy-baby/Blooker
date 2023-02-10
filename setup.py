994c7055d5d1750b757aa15ddd2d9d181ac07504
bigboy-baby <63435309+bigboy-baby@users.noreply.github.com>
2023-02-10 10:31:49 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,19 +3,16 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as f:
     page_description = f.read()
 
-with open("./requirements.txt") as f:
-    requirements = f.read().splitlines()
-
 setup(
     name="Blooker",
-    version="0.1",
+    version="0.0.1",
     author="Biggie",
     author_email="thugshaker@yahoo.com",
     description="Flood Blooket Games",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/bigboy-baby/"
+    url="https://github.com/bigboy-baby/",
     packages=find_packages(),
-    install_requires=requirements,
+    install_requires=['threading','requests','websocket'],
     python_requires='>=3',
 )
